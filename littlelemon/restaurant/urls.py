e23ce91788d3e8143c493e02a28f4c3190befb54
evis-i <eopari@gmail.com>
2024-07-30 00:43:22 +0200

    permission_classes, obtain auth token

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('menu/', MenuItemView.as_view()),
     path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    path('api-token-auth', obtain_auth_token),
 ]
 
